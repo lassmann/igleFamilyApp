f958240b6a152ca729ecac76af6a9d6432dac73e
Lucas Assmann <l.assmann.30@gmail.com>
2017-08-30 19:22:38 -0300

    Improving calendar access

--- src/pages/next-events/next-events.ts
+++ src/pages/next-events/next-events.ts
@@ -4,8 +4,8 @@ import { Calendar } from '@ionic-native/calendar';
 
 const NEXTEVENTS = [{
   title: 'Escuela de líderes',
-  startDate: '2017-08-30T21:00',
-  endDate: '2017-08-30T22:30',
+  startDate: '2017-09-30T21:00',
+  endDate: '2017-09-30T22:30',
   description: 'Es un espacio creado para la Formación Teológica y Ministerial de Líderes de Grupos de Amigos y Ministerios de la Iglesia de la Familia. Les brinda la posibilidad de compartir las problemáticas y encontrar consejo y guía a la luz de la Palabra de Dios; como así también estudiar y profundizar en las doctrinas fundamentales del Cristianismo.',
   address: 'Laprida 243, Presidencia Roque Sáenz Peña, Chaco, 3700, Argentina.',
   img: 'http://iglesiadelafamilia.com/wp-content/uploads/2017/08/4.png'
@@ -13,6 +13,13 @@ const NEXTEVENTS = [{
   title: 'Reunion de hombres',
   startDate: '2017-09-13T21:00',
   endDate: '2017-09-14T00:00',
+  description: 'Es un espacio creado para la Formación Teológica y Ministerial de Líderes de Grupos de Amigos y Ministerios de la Iglesia de la Familia. Les brinda la posibilidad de compartir las problemáticas y encontrar consejo y guía a la luz de la Palabra de Dios; como así también estudiar y profundizar en las doctrinas fundamentales del Cristianismo.',
+  address: 'Laprida 243, Presidencia Roque Sáenz Peña, Chaco, 3700, Argentina.',
+  img: 'http://iglesiadelafamilia.com/wp-content/uploads/2017/08/4.png'
+}, {
+  title: 'Reunion de mujeres',
+  startDate: '2017-09-09T21:00',
+  endDate: '2017-09-10T00:00',
   description: 'Es un espacio creado para la Formación Teológica y Ministerial de Líderes de Grupos de Amigos y Ministerios de la Iglesia de la Familia. Les brinda la posibilidad de compartir las problemáticas y encontrar consejo y guía a la luz de la Palabra de Dios; como así también estudiar y profundizar en las doctrinas fundamentales del Cristianismo.',
   address: 'Laprida 243, Presidencia Roque Sáenz Peña, Chaco, 3700, Argentina.',
   img: 'http://iglesiadelafamilia.com/wp-content/uploads/2017/08/4.png'

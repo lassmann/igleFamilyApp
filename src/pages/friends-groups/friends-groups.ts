ae22253e73179e8e6f3ba3339fe6a0b98698beec
Lucas Assmann <l.assmann.30@gmail.com>
2017-08-31 18:20:52 -0300

    google maps integration ready

--- src/pages/friends-groups/friends-groups.ts
+++ src/pages/friends-groups/friends-groups.ts
@@ -33,13 +33,16 @@ export class FriendsGroupsPage {
     this.mapElement = document.getElementById('map');
 
     let mapOptions: GoogleMapOptions = {
+      controls: {
+        myLocationButton: true,
+      },
       camera: {
         target: {
           lat: -26.787181,
           lng: -60.436987
         },
-        zoom: 13,
-        tilt: 30
+        zoom: 14,
+        tilt: 0
       }
     };
 
@@ -48,37 +51,55 @@ export class FriendsGroupsPage {
     this.map.one(GoogleMapsEvent.MAP_READY)
       .then(() => {
 
-        this.map.addMarker({
-          title: 'Iglesia de la Familia',
-          icon: 'blue',
-          animation: 'DROP',
-          position: {
-            lat: -26.787181,
-            lng: -60.436987
+      this.map.addMarkerCluster({
+        markers:[
+          {
+            title: 'Iglesia de la Familia',
+            snippet: 'Lider: Andres Neznazko, Horario: MARTES 20:30hs',
+            icon: 'blue',
+              animation: 'DROP',
+              position: {
+                lat: -26.787181,
+                lng: -60.436987
+              }
+          },
+          {
+            title: 'Casa: Raul Cernik',
+            icon: 'red',
+            snippet: 'Lider: Ricardo Baby, Horario: LUNES 20:30hs',
+            animation: 'DROP',
+            position: {
+              lat: -26.789805,
+              lng: -60.441503
+            }
           }
-        })
-          .then(marker => {
-            marker.on(GoogleMapsEvent.MARKER_CLICK)
-              .subscribe(() => {
-                alert('Lider: Andres Neznazko, Horario: MARTES 20:30hs');
-              });
-          });
+        ],
+        icons:[]
+      })
 
-        this.map.addMarker({
-          title: 'Casa: Raul Cernik',
-          icon: 'red',
-          animation: 'DROP',
-          position: {
-            lat: -26.789805,
-            lng: -60.441503
-          }
-        })
-          .then(marker => {
-            marker.on(GoogleMapsEvent.MARKER_CLICK)
-              .subscribe(() => {
-                alert('Lider: Ricardo Baby, Horario: LUNES 20:30hs');
-              });
-          });
+        // this.map.addMarker({
+        //   title: 'Iglesia de la Familia',
+        //   snippet: 'Lider: Andres Neznazko, Horario: MARTES 20:30hs',
+        //   icon: 'blue',
+        //   animation: 'DROP',
+        //   position: {
+        //     lat: -26.787181,
+        //     lng: -60.436987
+        //   }
+        // })
+        //
+        //
+        // this.map.addMarker({
+        //   title: 'Casa: Raul Cernik',
+        //   icon: 'red',
+        //   snippet: 'Lider: Ricardo Baby, Horario: LUNES 20:30hs',
+        //   animation: 'DROP',
+        //   position: {
+        //     lat: -26.789805,
+        //     lng: -60.441503
+        //   }
+        // })
+
 
       });
   }

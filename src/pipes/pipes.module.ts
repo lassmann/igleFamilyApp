0c3a48ed04da02b91ccb8bd3cb51c8f5b58d095c
Lucas Assmann <l.assmann.30@gmail.com>
2017-08-31 20:20:39 -0300

    youtube videos added

---
+++ src/pipes/pipes.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { YouTubePipe } from './../pipes/you-tube/you-tube';
+@NgModule({
+	declarations: [YouTubePipe],
+	imports: [],
+	exports: [YouTubePipe]
+})
+export class PipesModule {}

69f50d1d06e19753ee5fe014c54f4967c1f560db
Lucas Assmann <l.assmann.30@gmail.com>
2017-09-01 14:18:45 -0300

    Google Maps Location

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions
+} from '@ionic-native/google-maps';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  map: GoogleMap;
+  mapElement: HTMLElement;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private googleMaps: GoogleMaps) {}
 
+  ionViewDidLoad() {
+    this.loadMap();
   }
 
+  loadMap() {
+    this.mapElement = document.getElementById('igleLocation');
+
+    let mapOptions: GoogleMapOptions = {
+      controls: {
+        myLocationButton: true,
+      },
+      camera: {
+        target: {
+          lat: -26.787181,
+          lng: -60.436987
+        },
+        zoom: 15,
+        tilt: 0
+      }
+    };
+
+    this.map = this.googleMaps.create(this.mapElement, mapOptions);
+    this.map.one(GoogleMapsEvent.MAP_READY)
+      .then(() => {
+        this.map.addMarker({
+          title: 'Iglesia de la Familia',
+          snippet: 'Pastores: Carlos Cernik \nAndrés Neznajko\nHorarios: SÁBADOS: 20:30 (Adolescentes) 22:00hs (Jóvenes)\nDOMINGOS: 10:00 – 18:00 – 20:00hs',
+          icon: 'blue',
+          animation: 'DROP',
+          position: {
+            lat: -26.787181,
+            lng: -60.436987
+          }
+        })
+
+      });
+  }
 }

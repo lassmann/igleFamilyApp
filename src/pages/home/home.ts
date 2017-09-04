4c0c24cc472963d8f6fee7a1f792ace35bd88025
Lucas Assmann <l.assmann.30@gmail.com>
2017-09-03 22:50:27 -0300

    refactoring home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,20 +8,17 @@ import { FriendsGroupsPage } from '../../pages/friends-groups/friends-groups';
 import { GalleryPage } from '../../pages/gallery/gallery';
 import { CultsPage } from '../../pages/cults/cults';
 import { BlogPage } from '../../pages/blog/blog';
-import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions } from '@ionic-native/google-maps';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  map: GoogleMap;
-  mapElement: HTMLElement;
 
-  constructor(public navCtrl: NavController, private googleMaps: GoogleMaps, private emailComposer: EmailComposer) {
+  constructor(public navCtrl: NavController, private emailComposer: EmailComposer) {
   }
 
   ionViewDidLoad() {
-    this.loadMap();
   }
 
   contact() {
@@ -62,37 +59,4 @@ export class HomePage {
     this.navCtrl.push(CultsPage);
   }
 
-  loadMap() {
-    this.mapElement = document.getElementById('igleLocation');
-
-    let mapOptions: GoogleMapOptions = {
-      controls: {
-        myLocationButton: true,
-      },
-      camera: {
-        target: {
-          lat: -26.787181,
-          lng: -60.436987
-        },
-        zoom: 15,
-        tilt: 0
-      }
-    };
-
-    this.map = this.googleMaps.create(this.mapElement, mapOptions);
-    this.map.one(GoogleMapsEvent.MAP_READY)
-      .then(() => {
-        this.map.addMarker({
-          title: 'Iglesia de la Familia',
-          snippet: 'Pastores: Carlos Cernik \nAndrés Neznajko\nHorarios: SÁBADOS: 20:30 (Adolescentes) 22:00hs (Jóvenes)\nDOMINGOS: 10:00 – 18:00 – 20:00hs',
-          icon: 'blue',
-          animation: 'DROP',
-          position: {
-            lat: -26.787181,
-            lng: -60.436987
-          }
-        })
-
-      });
-  }
 }
